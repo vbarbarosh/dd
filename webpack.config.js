31b87e3aae1bc43581eaaa93934cc61c7f33fcd8
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-12-14 22:15:51 +0200

    compile for IE9+

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,20 @@ function render_config(mode)
             library: 'dd',
             libraryExport: 'default',
         },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                },
+            ],
+        },
     };
 }
 
